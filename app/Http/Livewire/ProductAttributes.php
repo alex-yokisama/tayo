9ea18ce9cee01860d9cd8cab3609fe38894df902
Alex Yokisama <alex.yokisama@gmail.com>
2020-12-21 11:57:33 +0100

    Added visual distribution of attributes by group and kind in product form

--- app/Http/Livewire/ProductAttributes.php
+++ app/Http/Livewire/ProductAttributes.php
@@ -4,17 +4,21 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Models\Attribute;
+use App\Models\AttributeGroup;
 
 class ProductAttributes extends Component
 {
     protected $listeners = ['categoryChanged' => 'updateAttributeList'];
     public $items;
+    public $kinds;
     public $productId;
     public $old;
 
     public function mount($productId, $old = null)
     {
         $this->items = collect([]);
+        $this->kinds = collect([]);
         $this->productId = $productId;
         $this->old = $old;
     }
@@ -28,10 +32,32 @@ class ProductAttributes extends Component
     {
         $this->errors = false;
         $category = Category::find($categoryId);
+        $this->kinds = collect([]);
         if ($category === null) {
-            $this->items = collect([]);
             return;
+        }       
+
+        $groups = AttributeGroup::orderBy('sort_order', 'ASC')->get();
+        $kinds = Attribute::kindsList();
+        foreach ($kinds as $kind_id => $kind_name) {
+            $tmpGroups = collect([]);
+            foreach ($groups as $group) {
+                $attributes = $category->attributes()->whereHas('group', function($query) use ($group) {
+                    $query->where('attribute_group_id', $group->id);
+                })->where('kind', $kind_id)->orderBy('sort_order', 'ASC')->get();
+                if ($attributes->count() > 0) {
+                    $tmpGroups->push((object)[
+                        'name' => $group->name,
+                        'attributes' => $attributes
+                    ]);
+                }
+            }
+            if ($tmpGroups->count() > 0) {
+                $this->kinds->push((object)[
+                    'name' => $kind_name,
+                    'groups' => $tmpGroups
+                ]);
+            }
         }
-        $this->items = $category->attributes()->orderBy('name', 'ASC')->get();
     }
 }
