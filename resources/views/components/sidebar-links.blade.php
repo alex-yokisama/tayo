d0f4cc5fefe7b1e9244fd49aa5dbd95ae6185450
Alex Yokisama <alex.yokisama@gmail.com>
2020-12-28 22:55:53 +0100

    Grouped items in sidebar menu

--- resources/views/components/sidebar-links.blade.php
+++ resources/views/components/sidebar-links.blade.php
@@ -1,7 +1,20 @@
 <ul>
-    @foreach ($sidebarLinks as $link)
-        <li class="{{ $link->active ? 'border-gray-200 border-l-2 text-white bg-gray-800' : 'text-gray-300 hover:text-white hover:bg-gray-800' }} uppercase ">
-            <a href="{{ $link->path }}" class="px-6 py-3 block">{{ $link->name }}</a>
+    @foreach ($sidebarLinks as $linkGroup)
+        <li x-data="{ show: {{ $linkGroup->active ? 'true' : 'false' }} }" class="uppercase">
+            <a href="#" class="uppercase px-6 py-3 flex flex-row space-x-2 flex-no-wrap items-center" x-bind:class="{ 'text-white bg-gray-800': show, 'text-gray-300 hover:text-white hover:bg-gray-800': !show }" x-on:click.prevent="show = !show">
+                <span>{{ $linkGroup->name }}</span>
+                </span>
+                    <x-common.arrow.sort-up class="border-white" x-show="show" />
+                    <x-common.arrow.sort-down class="border-white" x-show="!show" />
+                </span>
+            </a>
+            <ul x-show="show" class="mb-5 text-sm">
+                @foreach ($linkGroup->items as $link)
+                    <li class="{{ $link->active ? 'border-gray-200 border-l-2 text-white bg-gray-800' : 'text-gray-300 hover:text-white hover:bg-gray-800' }} pl-2">
+                        <a href="{{ $link->path }}" class="px-6 py-3 block">{{ $link->name }}</a>
+                    </li>
+                @endforeach
+            </ul>
         </li>
     @endforeach
 </ul>
